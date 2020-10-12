d869ec979148f542939484a2698023039b8ffcbc
Stanley <stanley.traub@gmail.com>
2020-10-12 14:47:05 -0700

    Initial Commit

---
+++ Messenger/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Messenger
+//
+//  Created by Stanley Traub on 10/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
